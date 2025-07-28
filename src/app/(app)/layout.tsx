e3e73ee71d3bcb116c8c87e7738115da4cb9b29f
Ihan Fuentes <aletoonx@gmail.com>
2025-07-28 06:26:49 +0000

    maybe you need to left all the foundations page, just make all the foote

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -12,7 +12,7 @@ export default function AppLayout({
       <div className="flex min-h-screen">
         <AppSidebar />
         <div className="flex flex-1 flex-col">
-          <SidebarInset className="flex flex-col">
+          <SidebarInset>
             <main className="flex-1 p-4 sm:p-6 lg:p-8">
               {children}
             </main>
