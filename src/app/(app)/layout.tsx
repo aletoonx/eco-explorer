3d9947d24c6ee26549955d5b3956aa62f2a9c428
Ihan Fuentes <aletoonx@gmail.com>
2025-07-28 06:16:59 +0000

    the footer still dont span all over the page, maybe the div is making it

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -16,8 +16,8 @@ export default function AppLayout({
             <main className="flex-1 p-4 sm:p-6 lg:p-8">
               {children}
             </main>
-            <Footer />
           </SidebarInset>
+          <Footer />
         </div>
       </div>
     </SidebarProvider>
